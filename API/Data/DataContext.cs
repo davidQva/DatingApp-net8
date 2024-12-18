06109a7cb01a790051dae639a668b81db8d07750
David Qvarnström <davidqvarnstrom@gmail.com>
2024-12-18 13:03:02 +0100

    end of section 2

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,10 @@
+using System;
+using API.Enteties;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class DataContext(DbContextOptions options) : DbContext(options)
+{
+    public required DbSet<AppUser> Users { get; set; }
+}
---
+++ API/Enteties/AppUser.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace API.Enteties;
+
+public class AppUser
+{
+    public int Id { get; set; }
+    public required string UserName { get; set; }
+}
